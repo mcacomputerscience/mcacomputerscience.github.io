fe7462b243a3c18d344b3f6989fa87e298b5c09a
Harinarayan Sriram <harinarayansriram@gmail.com>
2024-05-17 23:20:53 -0400

    Updated accordion and fixed to use minified css

--- js/accordion.js
+++ js/accordion.js
@@ -1,9 +1,5 @@
-const acc = document.getElementsByClassName("accordion-s");
-
-for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active-s");
-    const panel = this.nextElementSibling;
-    panel.style.maxHeight = panel.style.maxHeight ? null : `${panel.scrollHeight}px`;
-  });
-}
+$('.accordion-s').click(function () {
+  this.classList.toggle("active-s");
+  const panel = this.nextElementSibling;
+  panel.style.maxHeight = panel.style.maxHeight ? null : `${panel.scrollHeight}px`;
+});
