a3b442875942df9763575a837cd92f10a645193d
Harinarayan Sriram <harinarayansriram@gmail.com>
2024-05-26 17:52:31 -0400

    Added sponsors

--- js/accordion.js
+++ js/accordion.js
@@ -3,3 +3,15 @@ $('.accordion-s').click(function () {
   const panel = this.nextElementSibling;
   panel.style.maxHeight = panel.style.maxHeight ? null : `${panel.scrollHeight}px`;
 });
+
+$(".sponsor-slider").owlCarousel({
+  loop: true,
+  margin: 30,
+  nav: true,
+  navSpeed: 1e3,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 1e3,
+  navText: ['<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>'],
+  responsive: { 0: { items: 1 }, 600: { items: 3 }, 1e3: { items: 6 } }
+})
